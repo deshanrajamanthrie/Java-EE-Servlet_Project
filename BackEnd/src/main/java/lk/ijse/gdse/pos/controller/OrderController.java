b29c7dc0859657f7f45ad6ad8a27ff3f8b46f78c
deshanrajamanthrie <deshanrajamanthrie@gmail.com>
2023-08-23 22:44:50 +0530

    build my web

--- BackEnd/src/main/java/lk/ijse/gdse/pos/controller/OrderController.java
+++ BackEnd/src/main/java/lk/ijse/gdse/pos/controller/OrderController.java
@@ -25,37 +25,91 @@ import java.util.List;
 @WebServlet(urlPatterns = "/orders")
 public class OrderController extends HttpServlet {
     final String message = "";
-    PurchaseOrderBO purchaseOrderBO= (PurchaseOrderBO) BOFactory.getInstance().getBo(BOTypes.PURCHASE);
+    PurchaseOrderBO purchaseOrderBO = (PurchaseOrderBO) BOFactory.getInstance().getBo(BOTypes.PURCHASE);
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //-------------------------------------------------------------------------
-        saveOrder(req, resp);
+
+        try {
+            saveOrder(req, resp);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
 
     }
 
-    private void saveOrder(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        //   JsonReader reader = null;
-        JsonObjectBuilder obj = Json.createObjectBuilder();
+    private void saveOrder(HttpServletRequest req, HttpServletResponse resp) throws IOException, SQLException {
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonObject jsonObject = reader.readObject();
+        String id = jsonObject.getString("oId");
+        OrderDTO orderDTO = new OrderDTO(
+                id,
+                jsonObject.getString("customerId"),
+                Date.valueOf(jsonObject.getString("date"))
+        );
+        System.out.println(orderDTO.getOId() + " " + orderDTO.getCustomerId() + " " + orderDTO.getDate());
+        JsonArray orderdetail = jsonObject.getJsonArray("orderdetail");  //get reference from above object
+        ArrayList<OrderDetailDTO> list = new ArrayList<>();
+
+        for (JsonValue jsonValue : orderdetail) {
+            JsonObject jsonObject1 = jsonValue.asJsonObject();  //Json Value bind the Json Object
+            list.add(new OrderDetailDTO(
+                    id,
+                    jsonObject1.getString("itemCode"),
+                    jsonObject1.getJsonNumber("unitPrice").doubleValue(),
+                    jsonObject1.getInt("qty")
+            ));
+        }
+        orderDTO.setOrderDetailDTO(list);   //add the Json Array (Order Detail List)
+        if (orderDTO != null) {
+            try {
+                boolean isSavedOrder = purchaseOrderBO.saveOrder(orderDTO);
+                if (isSavedOrder) {
+                    JsonObjectBuilder response = Json.createObjectBuilder();
+                    response.add("code", 200);
+                    response.add("Message", "Save Succesed!");
+                    resp.getWriter().println(response.build());
+                } else {
+                    JsonObjectBuilder response = Json.createObjectBuilder();
+                    response.add("code", 400);
+                    response.add("Message", "Save Failed!");
+                    resp.getWriter().println(response.build());
+                }
+            } catch (Throwable e) {
+                JsonObjectBuilder response = Json.createObjectBuilder();
+                response.add("code", 500);
+                response.add("Message", "Error");
+                response.add("data", e.getLocalizedMessage());
+                resp.getWriter().println(response.build());
+
+            }
+
+
+        }
+
+
+
+        
+        
+
+        /*JsonObjectBuilder obj = Json.createObjectBuilder();
         try {
             JsonReader reader = Json.createReader(req.getReader());
             JsonObject jsonObject = reader.readObject();
             List<OrderDetailDTO> list = new ArrayList<>();
-            System.out.println("OrderDetail List :" + list);
             String orderId = jsonObject.getString("oId");
             OrderDTO orderDTO = new OrderDTO(
                     orderId,
                     jsonObject.getString("customerId"),
                     Date.valueOf(jsonObject.getString("date")
                     )
-
             );
-            //System.out.println("List :"+list);
+
             JsonArray orderDetailArray = jsonObject.getJsonArray("orderDetail");
             for (JsonValue jsonValue : orderDetailArray) {
                 JsonObject jsonObject1 = jsonValue.asJsonObject();
-                System.out.println("Hello :" + jsonObject1);
                 list.add(new OrderDetailDTO(
                         orderId,
                         jsonObject1.getString("itemCode"),
@@ -78,7 +132,7 @@ public class OrderController extends HttpServlet {
         } finally {
             resp.getWriter().println(obj.build());
         }
-
+*/
     }
 
     @Override
@@ -125,18 +179,17 @@ public class OrderController extends HttpServlet {
             List<OrderDTO> allOrders = purchaseOrderBO.getAll();
             JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
             JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
-            for (OrderDTO dto: allOrders) {
-                if(dto!=null) {
+            for (OrderDTO dto : allOrders) {
+                if (dto != null) {
                     objectBuilder.add("oId", dto.getOId());
                     objectBuilder.add("itemName", dto.getCustomerId());
                     objectBuilder.add("date", dto.getDate().toString());
                     arrayBuilder.add(objectBuilder);
-                 // objectBuilder.add(message,"Loading Succesed!");
-                }else{
-                    objectBuilder.add(message,"Loading Failed!");
+                } else {
+                    objectBuilder.add(message, "Loading Failed!");
                 }
             }
-           resp.getWriter().println(arrayBuilder.build());
+            resp.getWriter().println(arrayBuilder.build());
         } catch (Throwable e) {
             e.printStackTrace();
         }
